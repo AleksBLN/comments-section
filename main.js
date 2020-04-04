2e97f074e756068dae10efd464ebdc157357bc01
“Your <you@example.comblinovalexanders@gmail.com>
2020-04-04 22:39:02 +0300

    Fix delete button

---
+++ main.js
@@ -0,0 +1,73 @@
+const fetchData = () => {
+	fetch('php/getData.php').then((res) => res.json())
+	.then(response => {
+		if (response.length > 0) {
+			let output ='';
+			if ((response.length > 1) && (response[0].id < response[1].id)) {
+				response.reverse();
+			};
+			for(let i in response){
+				const img = (response[i].image) ? `<img src=${response[i].image} class="w-100 mb-2">` : '';
+				output += `
+				<div class="card mb-2 w-100">
+					<div class="card-header d-flex justify-content-between">
+						<p class="mb-n1">${response[i].name}</p>
+						<p class="mb-n1">${response[i].cur_date}</p>
+					</div>
+					<div class="card-body">
+						<p class="mb-2">${response[i].message}<br>
+						${img}
+						<div id="${response[i].id}" class="delete-button btn btn-outline-danger btn-sm py-0 float-right">Delete</div>
+						</p>
+					</div>
+				</div>`;
+			}
+			document.querySelector('#card-container').innerHTML = output;
+		} else {
+			document.querySelector('#card-container').innerHTML = '';
+		}
+	}).catch(error => console.log(error));
+};
+
+fetchData();
+
+const button = document.querySelector('#submit');
+
+button.addEventListener('click', () => {
+	const form = new FormData(document.querySelector('#commentForm'));
+	const request = new Request('php/addData.php', {
+		method: 'POST',
+		body: form
+	});
+	fetch(request)
+	.then(response => response.text())
+	.then(() => {
+
+		fetchData();
+
+	}).catch(error => console.log(error));
+
+});
+
+const commentsList = document.querySelector('#card-container');
+
+commentsList.addEventListener('click', (e) => {
+	e.preventDefault();
+	let cardId = event.target.id;
+	const formData = new FormData();
+	formData.append('id', cardId);
+	console.log(formData);
+	const request = new Request('php/deleteData.php', {
+		method: 'POST',
+		body: formData
+	});
+	console.log(request);
+	fetch(request)
+	.then(response => response.text())
+	.then(() => {
+
+		fetchData();
+
+	}).catch(error => console.log(error));
+
+});
--- php/addData.php
+++ php/addData.php
@@ -11,7 +11,7 @@ if($_FILES['image']['size'] != 0)
 	$fileName = $file['name'];
 	// $fileError = $file['error'];
 	$fileTmp = $file['tmp_name'];
-	$pathToStoreImg = "uploads/{$fileName}";
+	$pathToStoreImg = "../uploads/{$fileName}";
 	move_uploaded_file($fileTmp, $pathToStoreImg);
 }
 $sql = mysqli_query($conn, "INSERT INTO comment (name, message, cur_date, image) VALUES ('$name', '$comment', '$date', '$pathToStoreImg')");
